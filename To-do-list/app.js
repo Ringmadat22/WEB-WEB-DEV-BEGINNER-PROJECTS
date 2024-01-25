193d1451dd44f766d3d91a35fef006d310de90e4
Ring Madat <smithemma329@gmail.com>
2024-01-25 20:10:30 +0300

    main

--- To-do-list/app.js
+++ To-do-list/app.js
@@ -2,3 +2,13 @@ const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
+todoForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const newTask = todoInput.ariaValueMax;
+    if (newTask === '') {
+        alert('Please enter a task');
+        return;
+    }
+
+    todoInput.value = '';
+});
