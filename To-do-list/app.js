6240da77faa3b2d257e81819c82021853c8ce27f
Ring Madat <smithemma329@gmail.com>
2024-01-25 20:34:19 +0300

    main

--- To-do-list/app.js
+++ To-do-list/app.js
@@ -11,12 +11,38 @@ todoForm.addEventListener('submit', function(event) {
     }
 
     todoInput.value = '';
+    addTask(newTask);
 });
 
 function addTask(task) {
     const listItem = document.createElement('li');
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
     listItem.textContent = task;
 
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    listItem.appendChild(taskText);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    listItem.appendChild(deleteButton);
 
     todoList.appendChild(listItem);
+
+    checkBox.addEventListener('change', function() {
+        if (this.checked) {
+            taskText.style.textDecoration = 'line-through';
+        } else {
+            taskText.style.textDecoration = 'none';
+        }
+    });
+
+    deleteButton.addEventListener('clikc', function() {
+        todoList.removeChild(listItem);
+    });
+
+    editButtom.addEventListener('click', function() {
+        
+    })
 }
