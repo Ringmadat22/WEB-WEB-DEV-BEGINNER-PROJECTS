a4dd62c22898ba2f56c35aa8c0414e03873dbdd5
Ring Madat <smithemma329@gmail.com>
2024-01-31 10:11:12 +0300

    main

---
+++ counter-app/script.js
@@ -0,0 +1,8 @@
+const display = document.getElementsByClassName("counter-display");
+const increment = document.getElementsByClassName("increment");
+const decrement = document.getElementsByClassName("decrement");
+const reset = document.getElementsByClassName("reset");
+
+function addNumber() {
+    
+}
