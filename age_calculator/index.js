ebe90c30cfecfd67f2fedacc1c71e097f649a9e1
Ring Madat <smithemma329@gmail.com>
2024-05-22 09:59:18 +0300

    main

--- age_calculator/index.js
+++ age_calculator/index.js
@@ -1,1 +1,18 @@
-let currentDate = document.getElementById()
+let currDate= document.getElementById("currDate");
+let dateOfBirth = document.querySelector("#DOB");
+const CalcAge= document.getElementById("CalcAge");
+const displayAge= document.getElementById("displayAge");
+const Age= document.getElementById("age");
+var today = new Date();
+
+CalcAge.addEventListener("click",()=>{
+    var birthDate = new Date(dateOfBirth.value);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age = age - 1;
+    }
+displayAge.style.visibility="visible";
+Age.innerText=`You are ${age} years old.`
+});
+
